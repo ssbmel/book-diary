609fc1fe3b0aaebb90de4cd997f42d85a35d7745
서샛별 <seosaesbyeol@seosaesbyeol-ui-noteubug.local>
2024-10-23 18:18:14 +0900

    fix: 로그인 구현 완료

--- src/login-signup/GeneralLogin.tsx
+++ src/login-signup/GeneralLogin.tsx
@@ -1,27 +1,66 @@
-import { useNavigate } from 'react-router-dom';
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import supabase from "../supabase/supabaseClient";
 
 function GeneralLogin() {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
   const navigate = useNavigate();
 
   const goToSignup = () => {
-    navigate('/signup');
-  } 
+    navigate("/signup");
+  };
+
+  const loginHandler = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: emailRef.current?.value || "",
+      password: passwordRef.current?.value || "",
+    });
+    if (error) {
+      throw error;
+    }
+    alert("로그인 성공!");
+    navigate("/");
+  };
 
   return (
-    <div className='w-[250px] mx-auto grid gap-5 mt-[40px]'>
-        <input type="text" placeholder='아이디' className='p-1 px-3 border-b-2 rounded-sm shadow-sm'/>
-        <input type="password" placeholder='비밀번호' className='p-1 px-3 border-b-2 rounded-sm shadow-sm'/>
-        <button className='bg-red-950 text-white p-[6px] rounded-sm mt-[15px]'>로그인</button>
+    <form
+      onSubmit={loginHandler}
+      className="w-[250px] mx-auto grid gap-5 mt-[40px]"
+    >
+      <input
+        type="text"
+        ref={emailRef}
+        placeholder="이메일"
+        className="p-1 px-3 border-b-2 rounded-sm shadow-sm"
+      />
+      <input
+        type="password"
+        ref={passwordRef}
+        placeholder="비밀번호"
+        className="p-1 px-3 border-b-2 rounded-sm shadow-sm"
+      />
+      <button
+        type="submit"
+        className="bg-red-950 text-white p-[6px] rounded-sm mt-[15px]"
+      >
+        로그인
+      </button>
 
-        <div className='text-center grid gap-3 mt-[20px]'>
-            <button onClick={goToSignup} className='text-[14px]'>회원가입 하러 가기</button>
-            <div className='flex items-center mx-auto'>
-                <p className='text-gray-400 text-[12px] mr-1'>비밀번호를 잊으셨나요?</p>
-                <button className='text-[14px]'>비밀번호 찾기</button>
-            </div>
+      <div className="text-center grid gap-3 mt-[20px]">
+        <button onClick={goToSignup} className="text-[14px]">
+          회원가입 하러 가기
+        </button>
+        <div className="flex items-center mx-auto">
+          <p className="text-gray-400 text-[12px] mr-1">
+            비밀번호를 잊으셨나요?
+          </p>
+          <button className="text-[14px]">비밀번호 찾기</button>
         </div>
-    </div>
-  )
+      </div>
+    </form>
+  );
 }
 
 export default GeneralLogin;
